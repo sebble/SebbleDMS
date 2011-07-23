c477f5c8a34d1e1692113beaa72983bd23aa366f
Keith Newman <knewma@hotmail.com>
2011-07-23 23:25:09 +0100

    oops.  well that was embarrasing

--- Admin/demo/config.php
+++ Admin/demo/config.php
@@ -3,6 +3,6 @@
 #define('BASE','/var/www/N4/');
 
 User::$dir = dirname(__FILE__).'/../../Data/users/';
-Data_Admin::$dir = dirname(__FILE__).'admin/pages/';
-Controller_Admin::$template = dirname(__FILE__).'admin/admin.html';
+Data_Admin::$dir = dirname(__FILE__).'/pages/';
+Controller_Admin::$template = dirname(__FILE__).'/admin.html';
 #Data_Gallery::$dir = BASE.'gallery/';
